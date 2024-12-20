abf19bff22cadbcfd0920e18d6fc58a77cca9d9f
georgegathu <ggeorge.gathu@gmail.com>
2024-12-21 01:23:20 +0300

    C program to cheeck Prime Number

--- primeNumber.c
+++ primeNumber.c
@@ -2,12 +2,18 @@
 #include <math.h>
 
 int is_prime(int n) {
-    if (n <= 1) {
+    if (n <= 1) { 
         return 0; // Not prime
     }
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            return 0; // Not prime
+    if (n <= 3) { 
+        return 1; // 2 and 3 are prime
+    }
+    if (n % 2 == 0 || n % 3 == 0) { 
+        return 0; // Not prime if divisible by 2 or 3
+    }
+    for (int i = 5; i * i <= n; i += 6) { 
+        if (n % i == 0 || n % (i + 2) == 0) { 
+            return 0; // Not prime if divisible by i or i+2
         }
     }
     return 1; // Prime
