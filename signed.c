b9eadc76b069dd4fa0fc15a6c3da54a51cdf0e52
georgegathu <ggeorge.gathu@gmail.com>
2025-01-22 22:28:28 +0300

    Signed vs Unsigned variables

---
+++ signed.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int x = -10;
+    unsigned int y = 8;
+    if (x > y){
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than y\n");
+    }
+}
